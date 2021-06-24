a8bcfbc3ba79084f84d7a74b33a5d3dc9f54c6a7
iiitma <akinlesil@gmail.com>
2021-06-25 00:42:52 +0100

    fix(first release): first Release

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 
 // generate integer from range
-export function int(min: Number=0, max: Number=100): Number {
+export function int(min: Number = 0, max: Number = 100): Number {
     // add runtime check for use in JavaScript
     if (typeof max !== 'number' || typeof min !== 'number' ) {
        throw new Error('Invalid Inputs')
@@ -10,7 +10,7 @@ export function int(min: Number=0, max: Number=100): Number {
 }
 
 // generate float from range
-export function float(min: Number=0, max: Number=100, dp: Number=2): Number {
+export function float(min: Number = 0, max: Number = 100, dp: Number=2): Number {
     // add runtime check for use in JavaScript
     if (typeof max !== 'number' || typeof min !== 'number' || typeof dp !== 'number') {
        throw new Error('Invalid Inputs')
@@ -20,14 +20,14 @@ export function float(min: Number=0, max: Number=100, dp: Number=2): Number {
 }
 
 // generate integer Array from range and length
-export function intArray( count: Number=0, min: Number=0, max: Number=100): Number[] {
+export function intArray( count: Number = 0, min: Number = 0, max: Number = 100): Number[] {
     // add runtime check for use in JavaScript
     if (typeof max !== 'number' || typeof min !== 'number' || typeof count !== 'number'  ) {
        throw new Error('Invalid Inputs')
     }
     let result: Number[] = []
     // loop through "count" times
-    for(let i=0; i < count; i++){
+    for(let i = 0; i < count; i++){
         // add integer to result array
         result.push(int(min, max))
     }
@@ -36,14 +36,14 @@ export function intArray( count: Number=0, min: Number=0, max: Number=100): Numb
 }
 
 // generate float Array from range and length
-export function floatArray( count: Number=0, min: Number=0, max: Number=100, dp: Number=2): Number[] {
+export function floatArray( count: Number= 0, min: Number= 0, max: Number= 100, dp: Number= 2): Number[] {
     // add runtime check for use in JavaScript
     if (typeof max !== 'number' || typeof min !== 'number' || typeof dp !== 'number' || typeof count !== 'number'  ) {
        throw new Error('Invalid Inputs')
     }
     let result: Number[] = [];
     // loop through "count" times
-    for(let i=0; i < count; i++){
+    for(let i = 0; i < count; i++){
         // add float to result array
         result.push(float(min, max, dp))
     }
@@ -69,7 +69,7 @@ export function array(array: any[], count: Number=1): any[] {
 
     let result: any[] = [];
     // loop through "count" times
-     for(let i=0; i < count; i++){
+     for(let i = 0; i < count; i++){
          // generate random integer in range[0, array length]
          const rand = Math.round(Math.random()*(length-1))
          // add integer to resut array 
